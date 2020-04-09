47f7674ce104e940d1144e506d4d0cec1c614a6e
McKay Anhder <anhder.mckay@gmail.com>
2020-04-09 03:57:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
 if phrase == "I LOVE YOU GRANDMA!"
-  puts  "I LOVE YOU TOO PUMPKIN!"
+  "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase == phrase
-  puts "NO, NOT SINCE 1938!"
+  "NO, NOT SINCE 1938!"
   else
   "HUH?! SPEAK UP, SONNY!"
  end
